e64f1288cd3a19bace5581282e3eb0d592d2e707
dlowless <dlowless>
2004-04-04 16:53:24 +0000

    Nicer button test

--- web/css.php
+++ web/css.php
@@ -5,5 +5,6 @@ echo "A { text-decoration: none; color: rgb(0,0,0) }\n";
 echo "TABLE { font-family: verdana, helvetica, sans-serif; font-size:8pt ; border-spacing: 0pt }\n";
 echo "TD { font-family: verdana, helvetica, sans-serif; font-size:8pt ; border-spacing: 0pt ;border-padding: 0pt; font-weight: bold}\n";
 echo "input[type=text] {background-color: white; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n";
+echo "input[type=submit] {background-color: white; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n";
 echo "</style>";
 ?>
