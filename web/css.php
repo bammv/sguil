1804fd399419df153f7e2fc42d21df3bb8c23acd
dlowless <dlowless>
2004-04-04 20:27:00 +0000

    Added css to the normal body text for uniformity.

--- web/css.php
+++ web/css.php
@@ -6,5 +6,6 @@ echo "TABLE { font-family: verdana, helvetica, sans-serif; font-size:8pt ; borde
 echo "TD { font-family: verdana, helvetica, sans-serif; font-size:8pt ; border-spacing: 0pt ;border-padding: 0pt; font-weight: bold}\n";
 echo "input[type=text] {background-color: white; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n";
 echo "input[type=submit] {background-color: white; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n";
+echo "body {color: black; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n";
 echo "</style>";
 ?>
