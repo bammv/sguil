b6793111b4113c0d77de8fbc4f1a15110b91088d
mboman <mboman>
2004-04-05 16:33:59 +0000

    Coding style police in action.

--- web/css.php
+++ web/css.php
@@ -1,11 +1,13 @@
-<?
-echo "<style>\n";
-echo "A:hover { font-family: verdana , helvetica, sans-serif;text-decoration: underline }\n";
-echo "A { text-decoration: none; color: rgb(0,0,0) }\n";
-echo "TABLE { font-family: verdana, helvetica, sans-serif; font-size:8pt ; border-spacing: 0pt }\n";
-echo "TD { font-family: verdana, helvetica, sans-serif; font-size:8pt ; border-spacing: 0pt ;border-padding: 0pt; font-weight: bold}\n";
-echo "input {background-color: white; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n";
-//echo "input[type=submit] {background-color: white; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n";
-echo "body {color: black; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n";
-echo "</style>";
+<?php
+
+print("<style>\n");
+print("A:hover { font-family: verdana , helvetica, sans-serif;text-decoration: underline }\n");
+print("A { text-decoration: none; color: rgb(0,0,0) }\n");
+print("TABLE { font-family: verdana, helvetica, sans-serif; font-size:8pt ; border-spacing: 0pt }\n");
+print("TD { font-family: verdana, helvetica, sans-serif; font-size:8pt ; border-spacing: 0pt ; border-padding: 0pt; font-weight: bold}\n");
+print("input {background-color: white; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n");
+//print("input[type=submit] {background-color: white; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n");
+print("body {color: black; font-family: verdana, helvetica, sans-serif; font-size:8pt ;}\n");
+print("</style>");
+
 ?>
