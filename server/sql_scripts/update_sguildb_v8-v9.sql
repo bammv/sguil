7c986eef971823e5f4ee6390e7e1a67f131acb0d
bamm <bamm>
2004-06-24 20:25:11 +0000

    Fixed upgrade bug. Columns weren't in the correct order for the LOADs.  tks Rich.

--- server/sql_scripts/update_sguildb_v8-v9.sql
+++ server/sql_scripts/update_sguildb_v8-v9.sql
@@ -1,3 +1,25 @@
-ALTER TABLE sessions ADD ip_proto TINYINT UNSIGNED;
-UPDATE sessions SET ip_proto=6;
+CREATE TABLE sessions_tmp (
+  sid           INT UNSIGNED NOT NULL,
+  xid           BIGINT UNSIGNED NOT NULL,
+  start_time    datetime NOT NULL,
+  end_time      datetime NOT NULL,
+  src_ip        INT UNSIGNED NOT NULL,
+  dst_ip        INT UNSIGNED NOT NULL,
+  src_port      INT UNSIGNED NOT NULL,
+  dst_port      INT UNSIGNED NOT NULL,
+  ip_proto      TINYINT UNSIGNED NOT NULL,
+  src_pckts     BIGINT UNSIGNED NOT NULL,
+  dst_pckts     BIGINT UNSIGNED NOT NULL,
+  src_bytes     BIGINT UNSIGNED NOT NULL,
+  dst_bytes     BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (sid,xid),
+  INDEX begin (start_time),
+  INDEX end (end_time),
+  INDEX server (src_ip),
+  INDEX client (dst_ip),
+  INDEX sport (src_port),
+  INDEX cport (dst_port));
+INSERT INTO sessions_tmp SELECT sid, xid, start_time, end_time, src_ip, dst_ip, src_port, dst_port, 6, src_pckts, dst_pckts, src_bytes, dst_bytes FROM sessions;
+DROP TABLE sessions;
+RENAME TABLE sessions_tmp TO sessions;
 UPDATE version SET version="0.9", installed = now();
